c5851f85fcf3be964d502030a3398f1a18298046
Sebastijan Kolenko <sebastijan.kolenko@gmail.com>
2015-07-30 23:05:55 +0200

    scroll

--- assets/themes/koruza/js/main.js
+++ assets/themes/koruza/js/main.js
@@ -7,6 +7,22 @@ jQuery(function($) {'use strict',
 		});
 	});
 
+	// smooth scroll
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});	
+
 
 	// accordian
 	$('.accordion-toggle').on('click', function(){
