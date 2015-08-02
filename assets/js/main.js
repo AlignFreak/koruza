29b3f6606f64cc3a98cff1e54507a031af0c4974
Sebastijan Kolenko <sebastijan.kolenko@gmail.com>
2015-08-02 21:35:03 +0200

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ jQuery(function($) {'use strict',
 
 	//#main-slider
 	$(function(){
-		$('#main-slider .carousel').carousel({
+		$('#main-slider.carousel').carousel({
 			interval: 8000
 		});
 	});
