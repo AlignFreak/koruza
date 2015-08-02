9b15b0e4f60bc4315c72febe64f3ef8215990dc1
Sebastijan Kolenko <sebastijan.kolenko@gmail.com>
2015-08-02 22:19:41 +0200

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,24 +7,8 @@ jQuery(function($) {'use strict',
 		});
 	});
 
-	// smooth scroll
-	/*$(function() {
-		$('a[href*=#]:not([href=#])').click(function() {
-			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-				if (target.length) {
-					$('html,body').animate({
-						scrollTop: target.offset().top
-					}, 1000);
-					return false;
-				}
-			}
-		});
-	});	*/
-
 	//Initiat WOW JS
-	/*new WOW().init();*/
+	new WOW().init();
 
 	//Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
